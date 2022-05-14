b745974851f7fa00af024c5916b86d700bde323c
Tomas Costa <tomascostapp@gmail.com>
2022-05-14 12:41:59 -0300

    Tercer Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function iniciarMap() {
+    var coord = {lat: -32.8621065,lng: -68.89094}
+    var map = new google.maps.Map(document.getElementById('map'),{
+        zoom: 10,
+        center: coord
+    });
+    var marker = new google.maps.Marker({
+        position: coord,
+        map: map
+    });
+}
